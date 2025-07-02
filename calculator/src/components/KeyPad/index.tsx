34e9dd12c4b56455830678c6f24fc6c72dc90af6
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-07-02 14:31:22 +0100

    added text to buttons

--- calculator/src/components/KeyPad/index.tsx
+++ calculator/src/components/KeyPad/index.tsx
@@ -1,42 +1,69 @@
 import "./style.css"
-import { createElements } from "../../utils/tools.tsx"
+// import { createElements } from "../../utils/tools.tsx"
 import { Number } from "../Number/index.tsx"
 import { Operator } from "../Operator/index.tsx"
 // import React from "react"
 
 export function KeyPad(){
-    const numbers = 11;
-    const operators = [
-        "C", "CE", "(", ")",
-        "×", "-", "+", "=",
-        "÷", "%", "π", "√", 
-        "x!", "xʸ", "ln", "e",
-        "sin", "cos", "tan", "log",
-    ];
+    // const numbers = 11;
+    // const operators = [
+    //     "C", "CE", "(", ")",
+    //     "×", "-", "+", "=",
+    //     "÷", "%", "π", "√", 
+    //     "x!", "xʸ", "ln", "e",
+    //     "sin", "cos", "tan", "log",
+    //     "+/-"
+    // ];
 
-    const numberProps = {
-        key: 0,
-        number: "0"
-    }
+    // const numberProps = {
+    //     key: 0,
+    //     number: "0"
+    // }
 
-    const opBtns = createElements(operators.length, <Operator />);
-    const numBtns = createElements(numbers, <Number />, [], numberProps);
+    // const operatorProps = {
+    //     key: 0,
+    //     operator: ""
+    // }
+
+    // const opBtns = createElements(operators.length, <Operator />);
+    // const numBtns = createElements(numbers, <Number />, [], numberProps);
 
     return (<>
         <div id="keypad">
-            {...opBtns}
+            {/* {...opBtns} */}
             {/* <section id="number-btns">  */}
-            {numBtns[7]}
-            {numBtns[8]}
-            {numBtns[9]}
-            {numBtns[4]}
-            {numBtns[5]}
-            {numBtns[6]}
-            {numBtns[1]}
-            {numBtns[2]}
-            {numBtns[3]}
-            {numBtns[0]}
-            {numBtns[10]}
+            <Operator operator="e" />
+            <Operator operator="π" />
+            <Operator operator="(" />
+            <Operator operator=")" />
+            <Operator operator="sin" />
+            <Operator operator="cos" />
+            <Operator operator="tan" />
+            <Operator operator="log" />
+            <Operator operator="ln" />
+            <Operator operator="√" />
+            <Operator operator="x!" />
+            <Operator operator="xʸ" />
+            <Operator operator="+/-" />
+            <Operator operator="%" />
+            <Operator operator="C" />
+            <Operator operator="CE" />
+            <Number number="7" />
+            <Number number="8" />
+            <Number number="9" />
+            <Operator operator="÷" />
+            <Number number="4" />
+            <Number number="5" />
+            <Number number="6" />
+            <Operator operator="×" />
+            <Number number="1" />
+            <Number number="2" />
+            <Number number="3" />
+            <Operator operator="-" />
+            <Number number="0" />
+            <Number number="." />
+            <Operator operator="=" />
+            <Operator operator="+" />
             {/* </ section> */}
         </div>
     </>)
--- calculator/src/components/Operator/index.tsx
+++ calculator/src/components/Operator/index.tsx
@@ -1,9 +1,9 @@
 import "./style.css"
 
-export function Operator(){
+export function Operator(props?: any){
     return (<>
         <button className="operator">
-            
+            {`${props.operator}`}
         </button>
     </>)
 }
