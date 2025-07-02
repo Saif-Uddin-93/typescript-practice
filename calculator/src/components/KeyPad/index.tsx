61594c1c4588c09741bf77482971fdf154732a6b
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-07-02 13:49:32 +0100

    updated styles

--- calculator/src/components/KeyPad/index.tsx
+++ calculator/src/components/KeyPad/index.tsx
@@ -6,32 +6,38 @@ import { Operator } from "../Operator/index.tsx"
 
 export function KeyPad(){
     const numbers = 11;
-    const operators = 18;
+    const operators = [
+        "C", "CE", "(", ")",
+        "×", "-", "+", "=",
+        "÷", "%", "π", "√", 
+        "x!", "xʸ", "ln", "e",
+        "sin", "cos", "tan", "log",
+    ];
 
     const numberProps = {
         key: 0,
         number: "0"
     }
 
-    const opBtns = createElements(operators, <Operator />);
+    const opBtns = createElements(operators.length, <Operator />);
     const numBtns = createElements(numbers, <Number />, [], numberProps);
 
     return (<>
         <div id="keypad">
             {...opBtns}
-            <section id="number-btns"> 
-                {numBtns[7]}
-                {numBtns[8]}
-                {numBtns[9]}
-                {numBtns[4]}
-                {numBtns[5]}
-                {numBtns[6]}
-                {numBtns[1]}
-                {numBtns[2]}
-                {numBtns[3]}
-                {numBtns[0]}
-                {numBtns[10]}
-            </ section>
+            {/* <section id="number-btns">  */}
+            {numBtns[7]}
+            {numBtns[8]}
+            {numBtns[9]}
+            {numBtns[4]}
+            {numBtns[5]}
+            {numBtns[6]}
+            {numBtns[1]}
+            {numBtns[2]}
+            {numBtns[3]}
+            {numBtns[0]}
+            {numBtns[10]}
+            {/* </ section> */}
         </div>
     </>)
 }
