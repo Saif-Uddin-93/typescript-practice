1e7860f3e4860d12dd44974c948b1b6e5b66d050
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-07-02 12:22:29 +0100

    removed console logs

--- calculator/src/components/Number/index.tsx
+++ calculator/src/components/Number/index.tsx
@@ -1,7 +1,6 @@
 import "./style.css"
 
 export function Number(props?: any): React.ReactElement{
-    console.log("Number component rendered with number:", props.number);
     return (<>
         <button className="number">
             {`${props.number}`}
--- calculator/src/utils/tools.tsx
+++ calculator/src/utils/tools.tsx
@@ -3,12 +3,10 @@ import { cloneElement } from "react"
 export function createElements (instances: number, component: React.ReactElement, created: React.ReactElement[] = [], props?: any): React.ReactElement[] 
 {
     if (instances === created.length){
-        console.log(created)
         return created
     }
 
     function addNumber(){
-        console.log("length " + created.length)
         return {
             key: created.length + 1,
             number: created.length === 10 ? "." : created.length,
@@ -16,7 +14,6 @@ export function createElements (instances: number, component: React.ReactElement
     }
     if (props && 'number' in props) {
         props = addNumber()
-        console.log(`Debug: ${props.key} ${props.number}`)
     }
 
 
