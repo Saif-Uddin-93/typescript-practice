bc1a9d7602898d9e8bc533248d4a526a13c2f86c
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-07-02 14:38:01 +0100

    adjusted styling

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,13 +1,11 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import { Header } from "./components/Header/index.tsx";
+// import { Header } from "./components/Header/index.tsx";
 import { CalcBody } from "./components/CalcBody/index.tsx";
 
 export function App (){
   return (<>
-    <Header />
+    {/* <Header /> */}
     <main>
       <CalcBody />
     </main>
