0ee6c742ffe247824a76d7cc2aa35d4f904bde54
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-07-04 10:24:22 +0100

    tidied code

--- calculator/src/components/CalcDisplay/index.tsx
+++ calculator/src/components/CalcDisplay/index.tsx
@@ -11,8 +11,8 @@ export function CalcDisplay (){
     return (<>
         <div id="calc-display">
             <div id="display-container">
-                { context.displayedDigits === undefined || context.displayedDigits.length === 0 ? totalDigit  : null}
-                { context.displayedDigits.length < 17 ? context.displayedDigits : context.displayedDigits.slice(context.displayedDigits.length-16, context.displayedDigits.length)}
+                { context.displayedDigits === undefined || context.displayedDigits.length === 0 ? totalDigit  : null }
+                { context.displayedDigits.length < 17 ? context.displayedDigits : context.displayedDigits.slice(context.displayedDigits.length-16, context.displayedDigits.length) }
             </div>
         </div>
     </>)    
